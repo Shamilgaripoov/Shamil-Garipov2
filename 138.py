f36186d12138fdd2e6e27b47c227081f4a950ecd
Shamilgaripoov <113094713+Shamilgaripoov@users.noreply.github.com>
2022-12-15 21:19:13 +0300

    Update 138.py

--- 138.py
+++ 138.py
@@ -11,16 +11,18 @@ window = Tk()
 window.title("Программа")
 window.geometry("400x450")
 
-photo = PhotoImage(file = "ICU.gif")
-photobox = Label(window, image = photo)
-photobox.image = photo
-
+ph = PhotoImage(file = "ICU.gif")
+photobox = Label(window, image = ph)
+photobox.image = ph
 photobox.place(x = 100, y = 20, width = 200, height = 150)
-label = Label(text = "Введите имя: ")
-label.place(x=50, y=200, width=100, height=25)
+
+msg1 = Label(text = "Введите имя: ")
+msg1.place(x = 50, y = 200, width = 100, height = 25)
 selection = Entry(text = "")
 selection.place(x = 200, y = 200, width = 100, height = 25)
 selection.focus()
+
 button = Button(text = "Программа", command = programm)
 button.place(x = 150, y = 250, width = 100, height = 25)
+
 window.mainloop()
